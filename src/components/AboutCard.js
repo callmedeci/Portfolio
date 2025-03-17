e3d253353f2156fd4904c272f581252139a29436
callmedeci <callmedecim@gmail.com>
2025-03-17 09:28:56 -0700

    Fix About cards padding on mobile size

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -8,14 +8,14 @@ function AboutCard({ content: { title, description, icon }, index }) {
       initial={{ opacity: 0, y: 15 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.2, delay: index * 0.1 }}
-      className='group flex flex-col gap-3 rounded-2xl bg-zinc-200/10 px-10 py-7 shadow-sm ring shadow-zinc-400 ring-zinc-400 transition-all duration-300 hover:bg-zinc-200/50 hover:shadow-md dark:bg-zinc-800/10 dark:shadow-zinc-950 dark:ring-zinc-700/50 dark:hover:bg-zinc-800/50'
+      className='group flex flex-col gap-3 rounded-2xl bg-zinc-200/10 px-5 py-7 shadow-sm ring shadow-zinc-400 ring-zinc-400 transition-all duration-300 hover:bg-zinc-200/50 hover:shadow-md md:px-10 dark:bg-zinc-800/10 dark:shadow-zinc-950 dark:ring-zinc-700/50 dark:hover:bg-zinc-800/50'
     >
       <div className='flex w-full items-center gap-1'>
         {cloneElement(icon, {
           className:
             'size-5 md:size-7 text-zinc-800/60 transition-colors duration-300 group-hover:text-zinc-800/90 dark:text-zinc-200/60 dark:group-hover:text-zinc-200',
         })}
-        <h4 className='text-lg font-semibold text-zinc-800/60 transition-colors duration-300 group-hover:text-zinc-800/90 lg:text-xl xl:text-2xl dark:text-zinc-200/60 dark:group-hover:text-zinc-200'>
+        <h4 className='text-base font-semibold text-zinc-800/60 transition-colors duration-300 group-hover:text-zinc-800/90 sm:text-lg lg:text-xl xl:text-2xl dark:text-zinc-200/60 dark:group-hover:text-zinc-200'>
           {title}
         </h4>
       </div>
